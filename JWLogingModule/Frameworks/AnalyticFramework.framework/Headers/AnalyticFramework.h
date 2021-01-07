03f9b558e8d04dbf8d5ef73b2134b91a1d1e2995
wushiyu <wushiyu@mobimagic.com>
2021-01-07 15:31:28 +0800

    aaa

---
+++ JWLogingModule/Frameworks/AnalyticFramework.framework/Headers/AnalyticFramework.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticFramework.h
+//  AnalyticFramework
+//
+//  Created by 武世钰 on 2020/12/31.
+//  Copyright © 2020 武世钰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
